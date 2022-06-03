4021a9a106419d6e061c2fbbe02b4563b9651b2f
elbrus <elbrus.infosec@gmail.com>
2022-06-03 11:19:55 +0600

    init web app

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-2s3f(!ydi!7p^0)v!68@ebi60lb(+pk929w5p=cuz8=pd5q4^+'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
